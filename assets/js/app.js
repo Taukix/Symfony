c5060d33cb18ea8300525c2dffdd30b365d135d9
Tommy <tommy.boisseau@etu.unilim.fr>
2022-11-12 23:05:41 +0100

    5 fini

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import '../css/app.scss';
+import {Dropdown} from "bootstrap";
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../styles/app.css';
+document.addEventListener('DOMContentLoaded', () => {
+    enableDropdowns();
+});
 
-// start the Stimulus application
-import './bootstrap';
+const enableDropdowns = () => {
+    const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+    dropdownElementList.map(function (dropdownToggleEl) {
+        return new Dropdown(dropdownToggleEl)
+    });
+}
